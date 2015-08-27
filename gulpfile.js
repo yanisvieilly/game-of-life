4e773fd0b60a9dd1b7118485a9b3a7a19d473982
Yanis Vieilly <yanis.vieilly@gmail.com>
2015-08-27 22:20:21 +1200

    Add gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
-, coffee = require("gulp-coffee");
+var plumber = require('gulp-plumber');
+var coffee = require("gulp-coffee");
 
 gulp.task('build', function() {
   gulp.src('src/*.coffee')
-  .pipe(coffee())
-  .pipe(gulp.dest('dist'));
+    .pipe(plumber())
+    .pipe(coffee())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
